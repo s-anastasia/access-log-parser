172e351b6aecc439bc9711f9281620ee352c4039
ASergunina <kolom.anastasiya@gmail.com>
2025-06-28 15:07:57 +0300

    В main добавлен калькулятор

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        Scanner scanner = new Scanner(System.in);
+
+        // Вводим числп
+        System.out.print("Введите первое число: ");
+        int firstNumber = scanner.nextInt();
+        System.out.print("Введите второе число: ");
+        int secondNumber = scanner.nextInt();
+        // Вычисляем сумму, разность, произведение и частное
+        int sum = firstNumber + secondNumber;
+        int difference = firstNumber - secondNumber;
+        int product = firstNumber * secondNumber;
+        double quotient = (double) firstNumber / secondNumber;
+        // Выводим результат
+        System.out.println("Сумма: " + sum);
+        System.out.println("Разность: " + difference);
+        System.out.println("Произведение: " + product);
+        System.out.println("Частное: " + quotient);
+
+        scanner.close();
     }
 }
